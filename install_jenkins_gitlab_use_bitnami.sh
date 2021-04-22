76c69ddd0cd82d664fb96c9f62511e522e5821e3
xiaolixi <1974353987@qq.com>
2021-04-22 16:42:38 +0800

    Create install_jenkins_gitlab_use_bitnami.sh

---
+++ install_jenkins_gitlab_use_bitnami.sh
@@ -0,0 +1,5 @@
+https://downloads.bitnami.com/files/stacks/gitlab/8.5.1-0/bitnami-gitlab-8.5.1-0-linux-x64-installer.run
+https://downloads.bitnami.com/files/stacks/jenkins/2.89.4-1/bitnami-jenkins-2.89.4-1-linux-x64-installer.run
+
+https://downloads.bitnami.com/files/stacks/gitlab/8.8.4-0/bitnami-gitlab-8.8.4-0-linux-x64-installer.run
+https://downloads.bitnami.com/files/stacks/jenkins/2.150.3-1/bitnami-jenkins-2.150.3-1-linux-x64-installer.run
